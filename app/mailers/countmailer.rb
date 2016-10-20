cfc8acf5a132c1f4eb1d8dcbd398fe7d7bcc7505
Lucas <lucas.nowicki@gmail.com>
2016-10-20 07:17:31 +0000

    temat maila

--- app/mailers/countmailer.rb
+++ app/mailers/countmailer.rb
@@ -1,5 +1,6 @@
 class Countmailer < ApplicationMailer
-    def sendtocount
+    def sendtocount(id)
+        post = post.find(id)
         mail(to: 'lucas.nowicki@gmail.com', subject: 'Sample Email')
     end
 end
